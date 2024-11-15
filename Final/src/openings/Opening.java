485b73db7be7e52719ccc8826e903e89fc0e5880
fungi <glavnihonua@gmail.com>
2024-11-15 13:44:48 +0530

    doors and windows are working with minor bugs

---
+++ Final/src/openings/Opening.java
@@ -0,0 +1,14 @@
+package openings;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Opening extends JPanel {
+    Boolean delete_toggle = false;
+    Color room_color;
+    String horizontal_or_vertical;
+    Boolean opp;
+    protected void paintComponent(Graphics g){
+        super.paintComponent(g);
+    }
+}
